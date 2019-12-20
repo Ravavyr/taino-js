4b0432e6b25400711568643fe272982025ad5172
Ravavyr <rstamper@gmail.com>
2019-12-19 22:05:09 -0500

    updated docs with taino simple link

--- js/docs.js
+++ js/docs.js
@@ -41,7 +41,7 @@ class docsLoader {
                         </ul>
                         <h3>Coming Soon:</h3>
                         <ul class="l">
-                            <li>Taino_Simple: [a basic 5 flat pages starter site]</li>
+                            <li><a href="https://github.com/Ravavyr/taino-js/tree/taino_simple" target="_blank" rel="nofollow">Taino_Simple:</a> [a basic 2 flat pages starter site]</li>
                             <li>Taino_Ecomm: [built with ecommerce in mind, with cart logic and partial checkout]</li>
                             <li>Taino_Galleries: [built for portfolio sites]</li>
                             <li>Taino_Business: [built as a standard business website layout]</li>
